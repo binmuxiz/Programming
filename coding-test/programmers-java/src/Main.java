1a9eb226c288190268e817b46611754cab7023b7
leb001122 <leb001122@icloud.com>
2023-10-05 23:19:17 +0900

    23-10-05

--- coding-test/programmers-java/src/Main.java
+++ coding-test/programmers-java/src/Main.java
@@ -1,5 +1,8 @@
+import 코딩테스트입문.level0_특정문자제거하기;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        level0_특정문자제거하기 temp = new level0_특정문자제거하기();
+        System.out.println(temp.solution("abcdefgf", "f"));
     }
 }
