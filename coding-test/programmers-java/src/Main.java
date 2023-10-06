cfd4a471e51d3b5301c73431336548fff3d6b578
leb001122 <leb001122@icloud.com>
2023-10-06 21:01:27 +0900

    23-10-06

--- coding-test/programmers-java/src/Main.java
+++
@@ -1,8 +0,0 @@
-import 코딩테스트입문.level0_특정문자제거하기;
-
-public class Main {
-    public static void main(String[] args) {
-        level0_특정문자제거하기 temp = new level0_특정문자제거하기();
-        System.out.println(temp.solution("abcdefgf", "f"));
-    }
-}
---
+++ coding-test/programmers-java/src/코딩테스트입문/Main.java
@@ -0,0 +1,12 @@
+package 코딩테스트입문;
+
+public class Main {
+    public static void main(String[] args) {
+
+        level0_삼각형의완성조건1 p1 = new level0_삼각형의완성조건1();
+        System.out.println(p1.solution3(new int[]{1, 2, 3}));
+
+
+
+    }
+}
