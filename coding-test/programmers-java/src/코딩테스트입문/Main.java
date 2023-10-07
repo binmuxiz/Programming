9abe49154dc5381c7972b2e890aae58c6413d2b8
leb001122 <leb001122@icloud.com>
2023-10-08 01:02:00 +0900

    23-10-07

--- coding-test/programmers-java/src/코딩테스트입문/Main.java
+++ coding-test/programmers-java/src/코딩테스트입문/Main.java
@@ -3,10 +3,10 @@ package 코딩테스트입문;
 public class Main {
     public static void main(String[] args) {
 
-        level0_삼각형의완성조건1 p1 = new level0_삼각형의완성조건1();
-        System.out.println(p1.solution3(new int[]{1, 2, 3}));
+//        level0_삼각형의완성조건1 p1 = new level0_삼각형의완성조건1();
+//        System.out.println(p1.solution3(new int[]{1, 2, 3}));
 
-
-
+        level0_옹알이1 p2 = new level0_옹알이1();
+        System.out.println(p2.solution(new String[]{"aya", "yee", "u", "maa", "wyeoo"}));
     }
 }
