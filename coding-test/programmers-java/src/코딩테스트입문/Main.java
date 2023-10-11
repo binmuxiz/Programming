663fb38410d9d6b3a733aac1293f64ba61309ffa
leb001122 <leb001122@icloud.com>
2023-10-11 20:39:53 +0900

    23-10-11

--- coding-test/programmers-java/src/코딩테스트입문/Main.java
+++ coding-test/programmers-java/src/코딩테스트입문/Main.java
@@ -10,13 +10,17 @@ public class Main {
 //        System.out.println(p2.solution(new String[]{"aya", "yee", "u", "maa", "wyeoo"}));
 
 
-        int[][] arr = new int[3][2];
-        arr[0] = new int[]{-1,1};
-        arr[1] = new int[]{1,3};
-        arr[2] = new int[]{3,9};
-        level0_겹치는선분의길이 p3 = new level0_겹치는선분의길이();
-        System.out.println(p3.solution(arr));
+//        int[][] arr = new int[3][2];
+//        arr[0] = new int[]{-1,1};
+//        arr[1] = new int[]{1,3};
+//        arr[2] = new int[]{3,9};
+//        level0_겹치는선분의길이 p3 = new level0_겹치는선분의길이();
+//        System.out.println(p3.solution(arr));
 
-
+        level0_연속된수의합 p4 = new level0_연속된수의합();
+        int[] results = p4.solution(4, 14);
+        for (int i : results) {
+            System.out.print(i);
+        }
     }
 }
