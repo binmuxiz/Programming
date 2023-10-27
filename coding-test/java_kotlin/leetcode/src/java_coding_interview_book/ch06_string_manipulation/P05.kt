9d29d00a98385812e17c0754d7792e677fb06ea2
leb001122 <leb001122@icloud.com>
2023-10-27 19:11:26 +0900

    .

--- coding-test/java_kotlin/leetcode/src/java_coding_interview_book/ch06_string_manipulation/P05.kt
+++ coding-test/java_kotlin/leetcode/src/java_coding_interview_book/ch06_string_manipulation/P05.kt
@@ -2,16 +2,16 @@ package java_coding_interview_book.ch06_string_manipulation
 
 class P05 {
 
-    fun groupAnagrams(strs: Array<String>): MutableCollection<List<String>> {
-        var map = hashMapOf<String, List<String>>()
-
-
-
-    }
+//    fun groupAnagrams(strs: Array<String>): MutableCollection<List<String>> {
+//        var map = hashMapOf<String, List<String>>()
+//
+//
+//        return map
+//    }
 }
 
 fun main() {
 
     val p05 = P05()
-    p05.groupAnagrams(arrayOf("eat","tea","tan","ate","nat","bat"))
+//    p05.groupAnagrams(arrayOf("eat","tea","tan","ate","nat","bat"))
 }
