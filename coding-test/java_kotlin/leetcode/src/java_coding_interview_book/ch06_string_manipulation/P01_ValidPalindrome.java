e78eee803f41f5e1c221e20bb6f8edfde82d19b3
leb001122 <leb001122@icloud.com>
2023-10-27 22:34:09 +0900

    .

--- coding-test/java_kotlin/leetcode/src/java_coding_interview_book/ch06_string_manipulation/P01_ValidPalindrome.java
+++ coding-test/java_kotlin/leetcode/src/java_coding_interview_book/ch06_string_manipulation/P01_ValidPalindrome.java
@@ -7,31 +7,33 @@ public class P01_ValidPalindrome {
         System.out.println(solution1("1    "));
     }
 
+    // 2ms
     public static boolean solution1(String s) {
 
-        boolean answer = true;
-        int start = 0;
-        int end = s.length() - 1;
+        int h = 0;
+        int t = s.length() - 1;
 
-        while (start < end) {
-            if (!Character.isLetterOrDigit(s.charAt(start))) {
-                start++;
+        while (h < t) {
+            char ch1 = s.charAt(h);
+            char ch2 = s.charAt(t);
+
+            if (!Character.isLetterOrDigit(ch1)) {
+                h++;
                 continue;
-            } else if (!Character.isLetterOrDigit(s.charAt(end))) {
-                end--;
+            } else if (!Character.isLetterOrDigit(ch2)) {
+                t--;
                 continue;
             }
+            if (Character.toLowerCase(ch1) != Character.toLowerCase(ch2))
+                return false;
 
-            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
-                answer = false;
-                break;
-            }
-            start++;
-            end--;
+            h++; t--;
         }
-        return answer;
+
+        return true;
     }
 
+    // 23ms
     public static boolean solution2(String s) {
 
         String filteredStr = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
