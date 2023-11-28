7870e1dccb4ebc1151105b3f693b642360aa2142
leb001122 <leb001122@icloud.com>
2023-11-29 02:35:19 +0900

    .

--- DataStructure/Graph/src/zero_base/BFS_iterative.java
+++ DataStructure/Graph/src/zero_base/BFS_iterative.java
@@ -1,5 +1,10 @@
 package zero_base;
 
+
 public class BFS_iterative {
-    
+
+    // BFS 반복 구조는 큐를 이용하여 구현! (BFS는 재귀 사용 불가능)
+
+
 }
+x
--- Math/Math_04_Permutation/src/Practice1.java
+++ Math/Math_04_Permutation/src/Practice1.java
@@ -1,35 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // 1,2,3,4를 이용하여 세자리 자연수를 만드는 방법 (순서 0 , 중복 x)의 각 결과를 출력하시오
 // 방법 1
 public class Practice1 {
+    int n;
+    int r;
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4};
+        int[] arr = {1, 2, 3};
 
         Practice1 p = new Practice1();
-        p.permutation(arr, 0, 4, 3);
+        System.out.println(p.permute(arr,3, 2));
     }
 
-    private void permutation(int[] arr, int depth, int n, int r) {
+    private List<List<Integer>> permute(int[] nums, int n, int r) {
+        this.n = n;
+        this.r = r;
 
-        // 세자리수 출력
+        List<List<Integer>> result = new ArrayList<>();
+
+        return solution1(result, nums, 0);
+//        System.out.println(solution2(result, nums, 0));
+    }
+
+    private List<List<Integer>> solution1(List<List<Integer>> result, int[] nums, int depth) {
         if (depth == r) {
+            ArrayList<Integer> list = new ArrayList<>();
             for (int i = 0; i < r; i++) {
-                System.out.print(arr[i] + " ");
+                list.add(nums[i]);
             }
-            System.out.println();
-            return;
+            result.add(list);
+            return result;
         }
 
         for (int i = depth; i < n; i++) {
-            swap(arr, depth, i);
-            permutation(arr, depth + 1, n, r);
-            swap(arr, depth, i);
+            swap(nums, depth, i);
+            solution1(result, nums, depth + 1);
+            swap(nums, depth, i);
         }
+
+        return result;
     }
 
-    private void swap(int[] arr, int depth, int idx) {
-        int temp = arr[depth];
-        arr[depth] = arr[idx];
-        arr[idx] = temp;
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
     }
 }
