84601adddf833d51753b35984d5cb87e0159833f
leb001122 <leb001122@icloud.com>
2023-12-01 16:22:05 +0900

    .

---
+++ DataStructure/List/src/ListExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(10);
+
+        List<Integer> list2 = new LinkedList<>();
+    }
+}
