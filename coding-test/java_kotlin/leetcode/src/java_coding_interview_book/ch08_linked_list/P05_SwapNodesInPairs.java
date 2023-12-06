9954d45916941e7a3aeef48db20f943bd97ace80
leb001122 <leb001122@icloud.com>
2023-12-06 23:35:29 +0900

    .

---
+++ coding-test/java_kotlin/leetcode/src/java_coding_interview_book/ch08_linked_list/P05_SwapNodesInPairs.java
@@ -0,0 +1,11 @@
+package java_coding_interview_book.ch08_linked_list;
+
+public class P05_SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+
+
+        return null;
+    }
+}
